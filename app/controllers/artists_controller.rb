e750435b939cf7f80f2680d5b672eee564b2c361
mjsgreen01 <mjsgreen01@gmail.com>
2015-02-23 12:58:58 -0500

    added simple infinite-srcolling to artist.index.html

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -4,7 +4,7 @@ class ArtistsController < ApplicationController
   # GET /artists
   # GET /artists.json
   def index
-    @artists = Artist.all
+    @artists = Artist.paginate(:page => params[:page], :per_page => 100)
   end
 
   # GET /artists/1
